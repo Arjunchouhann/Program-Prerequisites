32c672163c5ee23d49f9a02c27a1143fa19557f0
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-08 12:48:02 +0530

    Program of converting Kilometers to Miles

---
+++ KtoMconvertor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class KtoMconvertor{
+	public static void main(String[]args){
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter Kilometers");
+		double Kilometers = sc.nextDouble();
+		System.out.println("Miles = " + (Kilometers*0.621371));
+		}
+	}
+	
